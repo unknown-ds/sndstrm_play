7cd999362f1b676cbea1d4ba1f06e3af8d0a268d
d.s <49014046+unknown-ds@users.noreply.github.com>
2019-04-26 15:32:23 +0900

    Add files via upload
    init commit

---
+++ Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using NAudio.Wave;
+using NAudio.CoreAudioApi;
+
+namespace sndstrm_play
+{
+    class Program
+    {
+        // メイン
+        static void Main(string[] args)
+        {
+            CWaveChunkReader wavChunkReader = new CWaveChunkReader();
+            BufferedWaveProvider bufferedWaveProvider;
+
+
+            // 入力ファイル
+            Console.WriteLine("input wave file..\n\n");
+            string wavFilePath = Console.ReadLine();
+            //string wavFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "test.wav");
+
+            // ファイル存在チェック
+            if (!File.Exists(wavFilePath))
+            {
+                throw new FileNotFoundException();
+            }
+
+            // ファイル読み込み開始
+            using (var _Fs = new FileStream(wavFilePath, FileMode.Open, FileAccess.Read))
+            {
+                // wavファイルのチャンク読み込み
+                wavChunkReader.ReadChunk(_Fs);
+                int _rate = wavChunkReader.SamplingRate;
+                int _bits = wavChunkReader.SamplingBit;
+                int _channels = wavChunkReader.Channels;
+
+                Console.WriteLine($"\n{_bits}[bit] {_rate}[Hz] {_channels}[ch] Loop={wavChunkReader.IsLoop()}");
+
+
+                // wavフォーマット
+                var _wavFormat = new WaveFormat(_rate, _bits, _channels);
+
+                // wavプロバイダーを生成
+                bufferedWaveProvider = new BufferedWaveProvider(_wavFormat);
+
+                // ボリューム調整用
+                var wavProvider = new VolumeWaveProvider16(bufferedWaveProvider) { Volume = 1.0f };
+
+                // 再生デバイスと出力先を設定(NAudioの用語でRender は出力、Capture は入力)
+                var mmDevice = new MMDeviceEnumerator().GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+
+                // タスクキャンセル用
+                var tokenSource = new CancellationTokenSource();
+                var token = tokenSource.Token;
+
+                // バッファ監視
+                Task _Task = TaskReadBuffer(_Fs, wavChunkReader, bufferedWaveProvider, token);
+
+                // 再生処理
+                using (var wavPlayer = new WasapiOut(mmDevice, AudioClientShareMode.Shared, false, 0))
+                {
+                    // 出力に入力を接続して再生開始
+                    wavPlayer.Init(wavProvider);
+                    wavPlayer.Volume = 0.2f;
+                    wavPlayer.Play();
+
+                    Console.WriteLine("\nPress Button Exit.");
+                    Console.ReadLine();
+
+                    wavPlayer.Stop();
+                }
+
+                // タスクキャンセル
+                tokenSource.Cancel();
+
+            }
+
+        }
+
+
+        //------------------------------------------------------------------
+        // バッファ監視タスク
+        static async Task TaskReadBuffer(
+            FileStream          _Fs,        // ファイルストリーム
+            CWaveChunkReader    _wavChunk,  // wavチャンク
+            BufferedWaveProvider _Provider, // バッファプロバイダー
+            CancellationToken   _token      // タスクキャンセル通知用
+            )
+        {
+            // 終了位置
+            int _StreamEnd = (_wavChunk.IsLoop())? _wavChunk.LoopEnd: _wavChunk.DataEnd;
+ 
+            // データ開始位置へシーク
+            _Fs.Seek(_wavChunk.DataStart, SeekOrigin.Begin);
+
+            var _IsLoop = true;
+            while (_IsLoop)
+            {
+                // 空きバッファサイズ（データを追加するサイズ）
+                int _EmptySize = _Provider.BufferLength - _Provider.BufferedBytes;
+                
+                // ストリーム位置チェック
+                if (_Fs.Position + _EmptySize > _StreamEnd)
+                {
+                    _EmptySize = _StreamEnd - (int)_Fs.Position;
+                    
+                    if (_wavChunk.IsLoop())
+                    {
+                        // ループ開始位置へ
+                        _Fs.Seek(_wavChunk.LoopStart, SeekOrigin.Begin);
+                    }
+                    else
+                    {
+                        _IsLoop = false;
+                    }
+                }
+
+                var _tmp = new byte[_EmptySize];
+                if (_EmptySize > 0)
+                {
+                    // ファイルから読み込み
+                    _Fs.Read(_tmp, 0, _EmptySize);
+                    // サンプルをバッファへ追加
+                    _Provider.AddSamples(_tmp, 0, _EmptySize);
+                }
+                
+                // タスクキャンセル要求チェック
+                if (_token.IsCancellationRequested) break;
+
+                await Task.Delay(100);
+            }
+
+        }
+
+    }
+}
---
+++ wavChunkReader.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace sndstrm_play
+{
+    //======================================================================
+    //----------------------------------------------------------------------
+    // wavファイルのチャンクデータを取得クラス
+    public class CWaveChunkReader
+    {
+        // 入力ファイル名
+        public string WaveFileName { set; get; }
+        // 走査済サイズ
+        public UInt32 ReadedSize { private set; get; } = 0;
+
+        // チャンネル数
+        public Int16 Channels { private set; get; } 
+        // サンプリングレート（Hz）
+        public Int32 SamplingRate { private set; get; }
+        // 量子化ビット数
+        public Int32 SamplingBit { private set; get; }
+        // サンプル数
+        public long SampleNum { private set; get; }
+        // サンプルフレーム数
+        public long NumSampleFrames { private set; get; }
+
+        // チャンク情報
+        public class CChunkInfo
+        {
+            public string Id { set; get; }      // チャンクID
+            public UInt32 Size { set; get; }     // サイズ
+        }
+        public List<CChunkInfo> ChunkInfoList { set; get; }
+
+        // データ情報
+        public Int32 DataStart { private set; get; }
+        public Int32 DataEnd { private set; get; }
+        public Int32 DataSize { private set; get; }
+
+        // ループ情報
+        public class CLoopInfo
+        {
+            public Int32 dwIdentifier { set; get; }    // ID
+            public Int32 dwType { set; get; }          // ループタイプ
+            public Int32 dwStart { set; get; }         // ループ開始位置
+            public Int32 dwEnd { set; get; }           // ループ終了位置
+            public Int32 dwFraction { set; get; }      // ループチューニング用
+            public Int32 dwPlayCount { set; get; }     // ループ回数(0=無限)
+        }
+        public List<CLoopInfo> LoopInfoList { set; get; }
+
+        // ループ情報があるか？
+        public bool IsLoop() => (LoopInfoList?.Count == 0) ? false : true;
+
+        // ループ開始位置と終了位置
+        public Int32 LoopStart { private set; get; }
+        public Int32 LoopEnd { private set; get; }
+ 
+
+        // プレイ時間(sec)取得
+        public float GetAllPlaySec() => (float)NumSampleFrames / (float)SamplingRate;
+
+
+        //=======================================
+        //---------------------------------------
+        // コンストラクタ
+        public CWaveChunkReader()
+        {
+            WaveFileName = null;
+            ChunkInfoList = new List<CChunkInfo>();
+            LoopInfoList = new List<CLoopInfo>();
+        }
+
+
+        //---------------------------------------
+        // チャンク読み込み
+        public void ReadChunk(
+            Stream      _Stream      // ストリーム
+            )
+        {
+            string _CkID = null;
+            UInt32 _CkSize = 0;
+
+            // バイナリ読み込み
+            var _Br = new BinaryReader(_Stream);
+            {
+                // Riffチャンクチェック
+                readChunk(_Br, ref _CkID, ref _CkSize);
+                char[] _FormType = _Br.ReadChars(4);
+
+                if ((_CkID != "RIFF") || string.Join("", _FormType) != "WAVE")
+                {
+                    throw new ApplicationException();
+                }
+
+                // チャンクデータ読み込み
+                UInt32 _FormChunkSize = _CkSize;
+                ReadedSize = 12;
+                while (ReadedSize < _FormChunkSize)
+                {
+                    ReadedSize += readChunk(_Br, ref _CkID, ref _CkSize);
+
+                    switch (_CkID.ToUpper())
+                    {
+                    case "FMT ": { readFormatChunk(_Br, _CkID, _CkSize); } break;
+                    case "DATA": { readDataChunk(_Br, _CkID, _CkSize); } break;
+                    case "SMPL": { readSmplChunk(_Br, _CkID, _CkSize); } break;
+                    default:
+                    {
+                        // 読み飛ばし
+                        _Stream.Seek(_CkSize, SeekOrigin.Current);
+                    }
+                    break;
+                    }
+
+                    // チャンク情報保持
+                    ChunkInfoList.Add(new CChunkInfo() { Id = _CkID.ToString(), Size = _CkSize });
+                    // 走査済サイズを加算
+                    ReadedSize += _CkSize;
+                }
+            }
+
+            // ストリーム位置リセット
+            _Stream.Seek(0, SeekOrigin.Begin);
+        }
+
+
+        //---------------------------------------
+        // チャンクを読む
+        protected UInt32 readChunk(
+            BinaryReader _Br,       // バイナリリーダー
+            ref string _Chunk,      // チャンク文字
+            ref UInt32 _Size        // サイズ
+            )
+        {
+            char[] _Ck = _Br.ReadChars(4);
+            _Chunk = string.Join("", _Ck);
+            _Size = _Br.ReadUInt32();
+
+            return (8);
+        }
+
+
+        //---------------------------------------
+        // FMTチャンク解析
+        protected UInt32 readFormatChunk(
+            BinaryReader _Br,       // バイナリリーダー
+            string _ChunkId,        // "FMT "
+            UInt32 _ChunkSize       // リニアPCなら必ず16
+            )
+        {
+            // フォーマットタグ、無圧縮なら1
+            var _wFormatTag = _Br.ReadUInt16();
+            // チャンネル数
+            var _wChannels = _Br.ReadUInt16();
+            // サンプリングレート（Hz）
+            var dwSamplesPerSec = _Br.ReadUInt32();
+            // データ速度(byte/sec)
+            var _dwAvgBytesPerSec = _Br.ReadUInt32();
+            // ブロックサイズ（Byte/sample×チャンネル数）
+            var _wBlockAlign = _Br.ReadUInt16();
+            // サンプルあたりのbit数(bit/sample) 8bit|16bir
+            var _wBitsPerSample = _Br.ReadUInt16();
+
+            UInt32 _ReadSize = 16;
+
+            Channels = (Int16)_wChannels;
+            SamplingBit = _wBitsPerSample;
+            SamplingRate = (Int32)dwSamplesPerSec;
+
+
+            // 読み出しサイズチェック
+            if (_ReadSize != _ChunkSize)
+            {
+                throw new ApplicationException();
+            }
+
+            return (_ReadSize);
+        }
+
+
+        //---------------------------------------
+        // DATAチャンク解析
+        protected UInt32 readDataChunk(
+            BinaryReader _Br,       // バイナリリーダー
+            string _ChunkId,        // "DATA"
+            UInt32 _ChunkSize       // 不定
+            )
+        {
+            var _SampleNum = _ChunkSize / (SamplingBit / 8);
+            var _NumSampleFrames = _SampleNum / Channels;
+
+            // サンプル数
+            SampleNum = _SampleNum;
+            // サンプルフレーム数
+            NumSampleFrames = _NumSampleFrames;
+
+            _Br.BaseStream.Seek(_ChunkSize, SeekOrigin.Current);
+
+            // データチャンク情報セット
+            DataStart = (int)ReadedSize;
+            DataSize = (int)_ChunkSize;
+            DataEnd = DataStart + DataSize;
+
+            return (_ChunkSize);
+        }
+
+
+        //---------------------------------------
+        // SMPLチャンク解析
+        protected UInt32 readSmplChunk(
+            BinaryReader _Br,       // バイナリリーダー
+            string _ChunkId,        // "SMPL"
+            UInt32 _ChunkSize       // 不定
+            )
+        {
+            Int32 _ReadByte = 0;
+
+            var dwManufacturer = _Br.ReadUInt32();
+            var dwProduct = _Br.ReadUInt32();
+            var dwSamplePeriod = _Br.ReadUInt32();      // 1サンプルの時間[nsec]
+            var dwMIDIUnityNote = _Br.ReadUInt32();
+            var dwMIDIPitchFraction = _Br.ReadUInt32();
+            var dwSMPTEFormat = _Br.ReadUInt32();
+            var dwSMPTEOffset = _Br.ReadUInt32();
+            var cSampleLoops = _Br.ReadUInt32();        // ループ情報数
+            var cbSamplerData = _Br.ReadUInt32();
+
+            _ReadByte += (4 * 9);
+
+            for (Int32 _Lp = 0; _Lp < cSampleLoops; _Lp++)
+            {
+                var dwIdentifier = _Br.ReadUInt32();    // ID
+                var dwType = _Br.ReadUInt32();          // ループタイプ
+                var dwStart = _Br.ReadUInt32();         // ループ開始位置
+                var dwEnd = _Br.ReadUInt32();           // ループ終了位置
+                var dwFraction = _Br.ReadUInt32();      // ループチューニング用
+                var dwPlayCount = _Br.ReadUInt32();     // ループ回数(0=無限)
+
+                LoopInfoList.Add(new CLoopInfo()
+                {
+                    dwIdentifier = (int)dwIdentifier,
+                    dwType = (int)dwType,
+                    dwStart = (int)dwStart,
+                    dwEnd = (int)dwEnd,
+                    dwFraction = (int)dwFraction,
+                    dwPlayCount = (int)dwPlayCount
+                });
+
+                _ReadByte += (4 * 6);
+            }
+
+            // 1サンプル当たりのバイト数
+            var _SampleByte = (SamplingBit / 8) * Channels;
+            // ループ位置計算（複数ループは見ていない）
+            if (IsLoop())
+            {
+                LoopStart = LoopInfoList[0].dwStart * _SampleByte + DataStart;
+                LoopEnd = LoopInfoList[0].dwEnd * _SampleByte + DataStart;
+            }
+
+            // 読み出しサイズチェック
+            if (_ReadByte != _ChunkSize)
+            {
+                throw new ApplicationException();
+            }
+
+            return (_ChunkSize);
+        }
+
+
+    }
+}
